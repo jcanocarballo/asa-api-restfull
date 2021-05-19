a9a373bdb42fbac8e07922f6b0b258f3efceb0ae
Julian Cano <jcanocarballo@gmail.com>
2021-05-19 17:50:06 -0500

    Se agrega swagger de auth y categorias

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const logger = require("./utils/logger");
 const uuid = require('uuid');
 const httpContext = require('express-http-context');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require(`./swagger/${process.env.SWAGGER_DOC}.json`);
 
 // Asignar puerto desde variables de entorno
 const PORT = process.env.PORT || 9797
@@ -44,6 +46,8 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/productos', require('./routes/productos'));
 //TODO: categorias
 app.use('/api/categorias', require('./routes/categorias'));
+//DOCS
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //Escuchar peticiones
 app.listen(PORT, () => {
   logger.info(`Servidor ejecutandose en en el puerto: ${PORT}`);
